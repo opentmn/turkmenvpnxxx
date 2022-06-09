a9834b26e7ffa8ecc00fc7bd586f183e456988ad
opentmn <102440180+opentmn@users.noreply.github.com>
2022-06-09 11:43:25 +0500

    Update ssylki.js

--- js/ssylki.js
+++ js/ssylki.js
@@ -1,22 +1,22 @@
 const s1 =
-  "https://drive.google.com/file/d/1uxmBVDQZuwnXTjq1K9r9Nv_x_CPai-Ng/view?usp=sharing";
+  "https://drive.google.com/file/d/1QZcajVLgzlp6R1Q6GUFDzGeCVoKbatKi/view?usp=sharing";
 const ssylka1 = document.createElement("a");
 ssylka1.setAttribute("href", s1);
 
 const s2 =
-  "https://drive.google.com/file/d/1fHyrSIyId5-ODciPq222CyMl9KRorgpO/view?usp=sharing";
+  "https://drive.google.com/file/d/1fxF7yjqgH8LsyYf_y8Z4kdsD-kVl0wU0/view?usp=sharing";
 const ssylka2 = document.createElement("a");
 ssylka2.setAttribute("href", s2);
 
 const s3 =
-  "https://drive.google.com/file/d/12dL9jYN2WPrzYNge9zSXYvsawRmveH6B/view?usp=sharing";
+  "https://drive.google.com/file/d/1YcuLbe-KxsDRvazRCYBahRCkwAITM0dd/view?usp=sharing";
 const ssylka3 = document.createElement("a");
 ssylka3.setAttribute("href", s3);
 
-const s4 =
-  "https://drive.google.com/file/d/1cSlGYV0KQe5J5Fk3g72LQulvtRVoROPy/view?usp=sharing";
-const ssylka4 = document.createElement("a");
-ssylka4.setAttribute("href", s4);
+// const s4 =
+//   "https://drive.google.com/file/d/1cSlGYV0KQe5J5Fk3g72LQulvtRVoROPy/view?usp=sharing";
+// const ssylka4 = document.createElement("a");
+// ssylka4.setAttribute("href", s4);
 
 const s5 =
   "https://drive.google.com/file/d/1F5MHLOlniDvVqA_ZXOjNK3O60Y1m1c85/view?usp=sharing";
